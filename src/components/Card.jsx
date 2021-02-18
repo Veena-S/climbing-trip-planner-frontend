25646ee13bcbe268dba8cb0c87233ff3dc3c3e3f
Kenrickles <kenrickles@gmail.com>
2021-02-18 09:34:54 +0800

    Base UI for home page done. Without Nav Bar

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Card.css'
+
+export default function Card({title, body}) {
+  return (
+    <div className="card-container">
+      <div className="image-container">
+
+      </div>
+     <div className="card-content">
+     <div className="card-title">
+        <h3>{title}</h3>
+      </div>
+      <div className="card-body">
+        <p>{body}</p>
+      </div>
+     </div>
+      <div className="btn">
+        <button>
+          <a>
+            View more
+          </a>
+        </button>
+      </div>
+    </div>
+  )
+}
