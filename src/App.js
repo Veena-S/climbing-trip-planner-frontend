553a83c0c762f802826cc1e9808f5b2adc6a21b4
jeremylim91 <jeremylim_91@live.com>
2021-02-18 15:00:20 +0800

    added navar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from "react";
 import HomePage from './components/HomePage'
+import NavbarComponent from './components/NavbarComponent.jsx'
 
 import { TripProvider } from "./store";
 
 function App() {
   return (
     <div className="App">
+      <NavbarComponent />
       <HomePage>
 
       </HomePage>
