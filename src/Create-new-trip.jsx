30819e5c23d7f0fc38cd7c959f5c2dcc404cbdc6
jeremylim91 <jeremylim_91@live.com>
2021-02-19 06:32:03 +0800

    installed dependencies for react-dates and wrote the code for it

--- src/Create-new-trip.jsx
+++ src/Create-new-trip.jsx
@@ -1,4 +1,12 @@
 import React, { useState } from "react";
+import "./styles.css";
+import "react-dates/initialize";
+import {
+  DateRangePicker,
+  SingleDatePicker,
+  DayPickerRangeController,
+} from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
 
 const FORM_FIELD_NAMES = {
   TRIP_NAME: "fTripName",
@@ -18,6 +26,22 @@ export default function CreateNewTrip() {
     setFormFields = newFormFields;
   };
 
+  // =================REACT-DATES==============================
+  const [dates, setDates] = useState({ startDate: null, endDate: null });
+  const [focusedInput, setFocusedInput] = useState(null);
+
+  const handleOnDateChange = ({ startDate, endDate }) => {
+    setDates({ startDate, endDate });
+  };
+
+  const { startDate, endDate } = dates;
+
+  if (startDate && endDate) {
+    console.log(startDate);
+    var formattedStartDate = startDate.format("dddd Do MMMM YYYY");
+    var formattedEndDate = endDate.format("dddd Do MMMM YYYY");
+  }
+  // =========================================================
   return (
     <div className="container">
       <div className="row">
@@ -47,7 +71,27 @@ export default function CreateNewTrip() {
         </div>
       </div>
       <div className="row">
-        <div className="col"></div>
+        <div className="col">
+          <div className="App">
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+            {startDate && endDate && (
+              <div>
+                <h4>from: {formattedStartDate}</h4>
+                <h4>to: {formattedEndDate}</h4>
+              </div>
+            )}
+            <DateRangePicker
+              startDate={startDate} // momentPropTypes.momentObj or null,
+              startDateId="start-date" // PropTypes.string.isRequired,
+              endDate={endDate} // momentPropTypes.momentObj or null,
+              endDateId="end-date" // PropTypes.string.isRequired,
+              onDatesChange={handleOnDateChange} // PropTypes.func.isRequired,
+              focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={(focusedInput) => setFocusedInput(focusedInput)} // PropTypes.func.isRequired,
+            />
+          </div>
+        </div>
       </div>
       <div className="row">
         <div className="col"></div>
