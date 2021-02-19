15499a0f2ecd9ad5cd9aaf7468ebacc41a7ac160
Kenrickles <kenrickles@gmail.com>
2021-02-19 14:55:13 +0800

    store update for tripIndex

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -45,9 +45,6 @@ export default function HomePage() {
   return (
     <>
     <div className="container">
-      <div className="btn-div">
-      <button> Create new Trip</button>
-      </div>
       <div className="container">
         <h2>Upcoming Trips</h2>
         <div className="row">
